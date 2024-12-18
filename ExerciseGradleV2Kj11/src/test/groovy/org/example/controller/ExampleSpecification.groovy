90d60a3923623e86b88be9ed0a15ee1b5f915621
Rodrigo Gadda <rodrigo.gadda@globallogic.com>
2024-12-18 17:13:14 -0300

    se pudo spock caso simple

---
+++ ExerciseGradleV2Kj11/src/test/groovy/org/example/controller/ExampleSpecification.groovy
@@ -0,0 +1,12 @@
+package org.example.controller
+
+
+import spock.lang.Specification
+
+class ExampleSpecification extends Specification{
+
+    def "should be a simple asset"() {
+        expect:
+        1 == 1
+    }
+}
